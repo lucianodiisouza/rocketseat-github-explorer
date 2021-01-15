3f35fdeb5c02f3d994a1dd2db708952fd830daf6
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-14 23:59:48 -0300

    config: project standart (prettier)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-      ff
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+);
 
 export default App;
