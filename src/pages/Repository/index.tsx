95f8fd9968c15e4d4984d1172c310fc5f722f4ca
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-16 00:29:29 -0300

    done: creating markup for repository info page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 
-interface RepositoryParams {
-  repository: string;
-}
+import { Header, RepositoryInfo, Issues } from './styles';
+
+// interface RepositoryParams {
+//   repository: string;
+// }
+
 const Repository: React.FC = () => {
-  const { params } = useRouteMatch<RepositoryParams>();
+  // const { params } = useRouteMatch<RepositoryParams>();
   return (
     <>
-      <h1>Repository {params.repository} </h1>
+      <Header>
+        <img src={logoImg} alt="Logo" />
+        <Link to="/">
+          <FiChevronLeft size={16} /> Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/10830068?s=400&u=8efbad27437b5aa7c436cb8cd3de95212f63ffdc&v=4"
+            alt="Roi"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Lorem, ipsum dolor.</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="/repositories/teste">
+          <div>
+            <strong>testes </strong>
+            <p>tests</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
